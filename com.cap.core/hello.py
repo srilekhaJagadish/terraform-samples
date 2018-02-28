e056f53454f1fd300657dde80649f611e4197679
Yerasi <srilekha.yerasi@capgemini.com>
2018-02-28 12:19:17 +0530

    Initial commit

---
+++ com.cap.core/hello.py
@@ -0,0 +1,16 @@
+print("Hello World")
+name=input("enter your name!!!")
+print(name)
+"""mutli line comments......
+hello world
+"""
+
+# hey this is single line comment
+print(name.upper())
+print(name.swapcase())
+
+str="Sri Lekha Yerasi"
+print(str.swapcase())
+print(str.title())
+seq = ("coca", "cola", "pepsi")
+print(seq.)
---
+++ com.cap.core/tuples.py
@@ -0,0 +1,10 @@
+tuple = ['Sri', 222, 'Lekha', 17.78]
+tinytuple = [123,'Discover']
+tup2 = [1, 2, 3, 4, 5, 6, 7, 8]
+#print(tuple)
+#print(tuple[0])
+print(tuple[1:3])
+#print(tuple[2:])
+#print(tuple[2:3])
+#print(tinytuple+tuple)
+print(tup2[2:5])
