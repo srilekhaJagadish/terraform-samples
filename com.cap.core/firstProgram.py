8ed386a5c20f5483a437d8dcf0706ccaba415abd
sryerasi <srilekha.yerasi@gmial.com>
2018-06-07 18:57:20 +0530

    Python Basics

---
+++ com.cap.core/firstProgram.py
@@ -0,0 +1,20 @@
+# greeting = 'Hello'
+# name = input('Please enter your name: ')
+# print(greeting + " " + name)
+splitString = ' This sting has been \n split \n over \n several \n lines '
+print(splitString)
+# ctrl + slash to comment
+tabbedString = '1 \t 2 \t 3 \t 4 \t 5 \t'
+print(tabbedString)
+# Printing the special characters
+print('To Print back slash "\\t" ')
+# Printing the single quotes
+print(' printing "double quotes"')
+# Printing single Quotes
+print("printing 'single quotes'")
+print('Hi this is lekha..., first name is \'Lekha\', second name is \'Yerasi\'')
+print("Hi this is lekha..., first name is \"Lekha\", second name is \"Yerasi\"")
+print("""This Strinf has 
+been splitted into 
+multiple lines""")
+print("""The pet shop owner said, "No, No e',s' resting""")
