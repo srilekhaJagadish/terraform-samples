aa81678795c1d45aadceea50bea4186050853068
Srilekha,YERASI <srilekha.yerasi@airbus.com>
2024-08-19 17:35:27 +0530

    use assume role

--- aws-resources/web-server.tf
+++ aws-resources/web-server.tf
@@ -1,5 +1,9 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::533267077438:role/deployer-role"
+    session_name = "terraform"
+  }
 }
 
 resource "aws_s3_bucket" "example" {
